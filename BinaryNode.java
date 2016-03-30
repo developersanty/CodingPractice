ea21e1705c9ce97e49ef57a46f5cb64b805c73eb
Santhosh Jebamani <santhosh.jebamani@cerner.com>
2016-03-30 18:54:14 -0500

    Unoptimized version of a binary search tree

---
+++ BinaryNode.java
@@ -0,0 +1,14 @@
+/**
+ * Created by sj034021 on 3/12/16.
+ */
+public class BinaryNode {
+  BinaryNode left;
+  BinaryNode right;
+  int data;
+
+  public BinaryNode(int data, BinaryNode left, BinaryNode right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+  }
+}
