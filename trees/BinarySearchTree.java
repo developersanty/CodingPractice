563a339ec9408ea4a2cbb000e8b27ef04638ff6b
Santhosh Jebamani <developersanty@gmail.com>
2016-05-31 19:17:16 -0500

    BinarySearchTree::Add - Original implementation

---
+++ trees/BinarySearchTree.java
@@ -0,0 +1,49 @@
+package trees;
+
+/**
+ * Created by sj034021 on 5/31/16.
+ */
+public class BinarySearchTree {
+  /*
+   *  Property:
+   *  Elements to the left of a node are lesser than that node
+   *  and elements to the right of a node are greater than that node
+   *
+   *  Operations: Add, Remove, Search
+   *  Access point: root node
+   *
+   *  Info: Just use data to across all operations
+  */
+
+  /*
+  * Implementation type: Recursive
+  * */
+  Node add(Node newNode, int data) {
+    if (newNode == null) {
+       newNode = new Node();
+       newNode.data = data;
+    } else {
+      if(data <= newNode.data) {
+        newNode.left = add(newNode.left, data);
+      } else {
+        newNode.right = add(newNode.right, data);
+      }
+    }
+
+    return newNode;
+  }
+
+  public static void main(String args[]) {
+    BinarySearchTree tree = new BinarySearchTree();
+
+    Node root = tree.add(null, 7);
+    root = tree.add(root, 4);
+    root = tree.add(root, 9);
+    root = tree.add(root, 3);
+    root = tree.add(root, 6);
+    root = tree.add(root, 8);
+    root = tree.add(root, 12);
+    root = tree.add(root, 12);
+    System.out.print("tree");
+  }
+}
---
+++ trees/BinaryTree.java
@@ -0,0 +1,7 @@
+package trees;
+
+/**
+ * Created by sj034021 on 5/31/16.
+ */
+public class BinaryTree {
+}
