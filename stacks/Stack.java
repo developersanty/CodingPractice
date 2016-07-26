214feb07451070a106bdf331aefd1b337bd9b7e1
Santhosh <developersanty@gmail.com>
2016-07-25 19:20:01 -0500

    Stacks: Reverse a stack using recursion

--- stacks/Stack.java
+++ stacks/Stack.java
@@ -19,9 +19,9 @@ public class Stack<T> {
 
   public void push(T data) {
 
-  if(size() == capacity) {
-    expandStack();
-  }
+    if(size() == capacity) {
+      expandStack();
+    }
 
     top++;
     elements[top] = data;
@@ -52,15 +52,42 @@ public class Stack<T> {
       System.out.println(elements[i]);
     }
   }
+  
+  // reversing a stack using recursion
+  public void reverse(Stack<T> stack) {
+
+    if(stack.size() == 0) {
+      return;
+    }
+
+    T topElement = stack.peek();
+    stack.pop();
+    reverse(stack);
+
+    pushToLast(topElement, stack);
+  }
+
+  public void pushToLast(T topElement, Stack<T> stack) {
+    if(stack.size() == 0) {
+      stack.push(topElement);
+      return;
+    }
+
+    T currentElement = stack.peek();
+    stack.pop();
+    pushToLast(topElement, stack);
+    stack.push(currentElement);
+  }
 
   public static void main(String args[]) {
     Stack<Integer> s= new Stack<Integer>();
 
     s.push(1);
-    s.push(1);
-    s.push(1);
-    s.push(1);
-    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.reverse(s);
     // System.out.print(s.peek());
     s.print();
   }
